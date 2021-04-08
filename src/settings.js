68c202ee4dc0530758d2c81db6de2780b5133fa9
Ali Zaraket <alizaraket600@gmail.com>
2021-04-08 17:42:11 +0300

    added app url to endpoints

--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,5 @@
-export const AUTH_ENDPOINT = "http://localhost:8000/api/users/";
-export const ROOMS_ENDPOINT = "http://localhost:8000/api/rooms/";
-export const MESSAGES_ENDPOINT = "http://localhost:8000/api/messages/";
-export const POSTS_ENDPOINT = "http://localhost:8000/api/posts/";
+export const AUTH_ENDPOINT = "https://z-ch-at.herokuapp.com/api/users/";
+export const ROOMS_ENDPOINT = "https://z-ch-at.herokuapp.com/api/rooms/";
+export const MESSAGES_ENDPOINT = "https://z-ch-at.herokuapp.com/api/messages/";
+export const POSTS_ENDPOINT = "https://z-ch-at.herokuapp.com/api/posts/";
 export const SERVER_ENDPOINT = "https://zchatserver.herokuapp.com/";
